585e6d00d896ecae1dfca55321781e6fa104350b
leokingue <ebangkingue@icloud.com>
2022-11-01 11:26:40 -0700

    update

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" module models """
+
+
+from models.engine.file_storage import
+
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -15,16 +15,27 @@ self.__str__: should print: [<class name>] (<self.id>) <self.__dict__>
 
 """
 import uuid
-from datetime import date, datetime
+from datetime import datetime
+import models
 
 
 class BaseModel:
     """ Implementation of the BaseModel class """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ The constructor of the BaseModel class """
-        self.id = str(uuid.uuid4())
-        self.create_at = datetime.now()
-        self.update_at = datetime.now()
+        if kwarg:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """ Show current object information """
@@ -32,12 +43,13 @@ class BaseModel:
 
     def save(self):
         """ Updates the public instance attribute updated_at with the curren"""
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ Returns a dictionary containing all keys/values of __dict__"""
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
-        new_dict['create_at'] = datetime.isoformat(self.create_at)
-        new_dict['update_at'] = datetime.isoformat(self.update_at)
+        new_dict['created_at'] = datetime.isoformat(self.created_at)
+        new_dict['updated_at'] = datetime.isoformat(self.updated_at)
         return new_dict
