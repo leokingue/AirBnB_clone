369b25998adeda546c69b684a8966854cb016560
leokingue <ebangkingue@icloud.com>
2022-11-12 13:10:17 +0100

    update the base_model.py file

--- models/base_model.py
+++ models/base_model.py
@@ -1,55 +1,67 @@
-#!/usr/bin/python
-"""
-The BaseModel class is a parent class for future objects in our AirBnB
-It will have the following attributes:
-    id: string - assign with an uuid when an instance is created
-    created_at: datetime - assign with the current datetime when an instance
-    updated_at: datetime - assign with the current datetime when an instance
-
-Public instance methods:
-save(self): updates the public instance attribute updated_at with the curr
-to_dict(self): returns a dictionary containing all keys/values of __dict__
-
-A method inherited from the redefined Object super class:
-self.__str__: should print: [<class name>] (<self.id>) <self.__dict__>
-
-"""
+#!/usr/bin/python3
 import uuid
 from datetime import datetime
-import models
+from models import storage
+"""
+The BaseModel class is a parent class for future objects in our AirBnB web project.
+ It will have the following attributes:
+
+        id: string - assign with an uuid when an instance is created
+        created_at: datetime - assign with the current datetime when an instance is created
+        updated_at: datetime - assign with the current datetime when an instance is created,
+        and it will be updated every
+         time you change your object
+
+ A method inherited from the redefined Object super class:
+
+        self.__str__: should print: [<class name>] (<self.id>) <self.__dict__>
+
+ Public instance methods:
+        save(self): updates the public instance attribute updated_at with the current datetime
+        to_dict(self): returns a dictionary containing all keys/values of __dict__ of the instance
+
+"""
+'''
+IMPORTANT:To reload an object use the storage instance on the reload method
+            storage.reload()
+            <storage> is an instance of FileStorage
+            Respect The rules !
+'''
 
 
 class BaseModel:
     """ Implementation of the BaseModel class """
     def __init__(self, *args, **kwargs):
         """ The constructor of the BaseModel class """
-        if kwarg:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        storage.new(self)
+        if kwargs is not None:
             for key, value in kwargs.items():
                 if key == "created_at":
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    self.created_at = str(datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                 if key == "updated_at":
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+                    self.updated_at = str(datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
 
     def __str__(self):
         """ Show current object information """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """ Updates the public instance attribute updated_at with the curren"""
+        """ Updates the public instance attribute updated_at with the current datetime """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
-        """ Returns a dictionary containing all keys/values of __dict__"""
+        """ Returns a dictionary containing all keys/values of __dict__ of the instance"""
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = datetime.isoformat(self.created_at)
-        new_dict['updated_at'] = datetime.isoformat(self.updated_at)
+        new_dict['created_at'] = str(datetime.fromisoformat(str(self.created_at)))
+        new_dict['updated_at'] = str(datetime.fromisoformat(str(self.updated_at)))
         return new_dict
+
+
+if __name__ == "__main__":
+    b1 = BaseModel()
+    b1.save()
