86443e243e286b77a7a3263b947cb991a268b9c6
leokingue <ebangkingue@icloud.com>
2022-11-12 13:03:47 +0100

    add the ./models/__init__.py file

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-""" module models """
+from models.engine import customserializer
+from models.engine.file_storage import FileStorage
 
-
-from models.engine.file_storage import
 
 storage = FileStorage()
 storage.reload()
